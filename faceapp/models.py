5daedc3dce4fbd0eeb184c13c245bd9400f10c80
MohammedRaghib <mohammedmraghib@gmail.com>
2025-06-11 17:38:49 +0300

    changed person id to not be pk and be a charfield

--- faceapp/models.py
+++ faceapp/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.utils import timezone
 
 class Worker(models.Model):
-    person_id = models.AutoField(primary_key=True)
+    person_id = models.CharField(null=True, blank=True) 
     name = models.CharField(max_length=100)
     face_encoding = models.JSONField()
     role = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
+
     
 class Equipment(models.Model): 
     name = models.CharField(max_length=100)
