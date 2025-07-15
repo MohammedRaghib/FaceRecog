784bf535937cfc65be17c74d4c654b0b652ff385
MohammedRaghib <mohammedmraghib@gmail.com>
2025-07-15 09:38:02 +0300

    Update models.py

--- faceapp/models.py
+++ faceapp/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 class Worker(models.Model):
+    person_id = models.CharField(null=True, blank=True) 
     name = models.CharField(max_length=100)
     face_encoding = models.JSONField()
+    role = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
